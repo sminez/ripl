4c4902db7b12343dac313f152a28d2f30f0b0e7d
sminez <innes.morrison@cocoon.life>
2016-08-08 14:35:54 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='ripl',
+    version="0.0.2",
+    description="A LISP that runs on the python VM",
+    url="https://github.com/sminez/ripl",
+    author="Innes Anderson-Morrison",
+    author_email='innes.morrison@cocoon.life',
+    install_requires=[
+        'pygments==2.1.3',
+        'prompt_toolkit==1.0.0',
+    ],
+    packages=find_packages(),
+    package_dir={'ripl': 'ripl'},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ripl = ripl.cli:main',
+        ]
+    },
+)
